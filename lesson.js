e9091400a6b1aa819822afccb96b9ddb1ba01730
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-09-02 14:18:55 +0300

    lesson

--- lesson.js
+++ lesson.js
@@ -1,145 +1,142 @@
-const obj = { a: 1, b: 2, c: 3 };
+const str = "12345   6789";
+const username = "Taras";
+const greeting = "Hello";
 
-for (let key in obj) {
-  console.log("key", key, "value", obj[key]);
-}
-const arr = [1, 2, 3];
+console.log("str.length==>>", str.length);
 
-for (let key in arr) {
-  console.log("key", key, "value", arr[key]);
-}
+// slice
+let slice1 = str.slice(); // all
+let slice2 = str.slice(2); // from element 3 to end
+let slice3 = str.slice(2, 5); // from element 3 to element 5
+let slice4 = str.slice(-2); // from end element 8 to end
 
-for (let value of arr) {
-  console.log(value);
-}
+// substring
+// doesn't work with negative numbers
+let substring1 = str.substring(2); // from element 3 to end
+let substring2 = str.substring(2, 5); // from element 3 to element 5
 
-// don't work with objects
-// for (let value of obj) {
-//   console.log(value);
-// }
+// substr
+// doesn't work with negative numbers
+let substr1 = str.substr(2); // from element 3 to end
+let substr2 = str.substr(2, 3); // from element 3 with length 3
 
-const objKeys = Object.keys(obj);
-console.log("🚀 ~ file: main.js:17 ~ objKeys:", objKeys);
-// ["a", "b", "c"];
-const objValues = Object.values(obj);
-console.log("🚀 ~ file: main.js:19 ~ objValues:", objValues);
-// [1, 2, 3];
-const objEntries = Object.entries(obj);
-console.log("🚀 ~ file: main.js:21 ~ objEntries:", objEntries);
-// [
-//   ["a", 1],
-//   ["b", 2],
-//   ["c", 3],
-// ];
-myFunc();
+// concat
+// concatenates strings together
+const concat = greeting.concat(" ", username);
 
-function myFunc() {
-  for (let i = 0; i < 10; i++) {
-    console.log(i);
-  }
-}
+// replace
+const replaced = username.replace("a", "t"); //replaces first 'a' with 't'
+const replacedRegex = username.replace(/a/, "t"); //replaces first math of regex with 't'
 
-const myFuncExpression = function () {
-  for (let i = 0; i < 10; i++) {
-    console.log("myFuncExpression");
+// replaceAll
+const replacedAll = username.replaceAll("a", "t"); //replaces all 'a' with 't'
+const replacedAllRegex = username.replaceAll(/a/g, "t"); //replaces first math of regex with 't' g flag is mandatory
+
+// toLowerCase or toLocaleLowerCase
+const toLowerCase = username.toLowerCase(); // makes all letters small
+const toLocaleLowerCase = username.toLocaleLowerCase(); // makes all letters small (works great with different languages)
+
+// toUpperCase or toLocaleUpperCase
+const toUpperCase = username.toUpperCase(); // makes all letters big
+const toLocaleUpperCase = username.toLocaleUpperCase(); // makes all letters big (works great with different languages)
+
+// trim
+// deletes whitespace at the beginning and end
+const whiteSpaces = "   aaa   ";
+const trim = whiteSpaces.trim();
+const trimStart = whiteSpaces.trimStart(); // cuts only whitespace at the beginning
+const trimEnd = whiteSpaces.trimEnd(); // cuts only whitespace at the end
+
+// padStart padEnd
+// adds a symbols on start or end of str
+const padStart1 = username.padStart(1, "="); // if length of a str is bigger then number in function then nothing is added
+const padStart2 = whiteSpaces.padStart(10, "="); // if number in function is bigger then length of str then elements are added at the beginning
+const padEnd1 = username.padEnd(1, "="); // if length of a str is bigger then number in function then nothing is added
+const padEnd2 = whiteSpaces.padEnd(10, "="); // if number in function is bigger then length of str then elements are added at the end
+
+// get letter
+const letter1 = username[2];
+const letter2 = username.charAt(2);
+const letterCode = username.charCodeAt(2); // return code of a char, see ascii table
+
+// split
+// transforms string into array
+const split = username.split(""); // splits every element as separate element in array
+
+// search
+username.indexOf("a"); // returns index of first found element or -1 if no element found
+username.indexOf("a", 3); // returns index of first found element from position 3 or -1 if no element found
+username.lastIndexOf("a"); // returns index of first found element nut from end or -1 if no element found
+username.search("a"); // returns index of first found element or -1 if no element found
+username.search(/a/); // returns index of first found element or -1 if no element found
+username.match(/a/g); // returns arr of found elements or null if no element found
+username.includes("a"); // return true or false
+username.startsWith("T"); // return true or false
+username.endsWith("s"); // return true or false
+
+const printPyramid = (rows) => {
+  let starsLength = 1;
+  let cagesLength = rows - starsLength;
+
+  for (let i = 0; i < rows; i++) {
+    const cages = "".padStart(cagesLength, "#");
+    const stars = "".padStart(starsLength, "*");
+    starsLength += 2;
+    cagesLength--;
+    console.log(`${cages}${stars}${cages}`);
   }
 };
-myFuncExpression();
 
-const str = "outside 1 key";
-const arrowFunc = (value1, value2, value3) => {
-  const str = "key";
-  value3.d = 4;
-  for (let key of objKeys) {
-    console.log(str, key);
+const name = "taras";
+
+const capitalize = (str) => {
+  const arr = str.split("");
+  let capitalized = "";
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      capitalized = arr[i].toUpperCase();
+    } else {
+      capitalized += arr[i];
+    }
   }
+  return capitalized;
 };
 
-arrowFunc(1, str, obj);
-console.log(obj);
-
-const arrowWith1Arg = value => {
-  console.log(value);
+const charTransform = (str) => {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    const code = str.charCodeAt(i);
+    if (code >= 65 && code <= 90) {
+      result += str[i].toLowerCase();
+    } else {
+      result += str[i].toUpperCase();
+    }
+  }
+  return result;
 };
 
-arrowWith1Arg(12)
+charTransform("HeLlO");
 
-const multiply = (number, multiplier = 2) => {
+const vowels = {
+  a: "a",
+  e: "e",
+  o: "o",
+  i: "i",
+  u: "u",
+};
 
-  // if (number > 10) {
-  //   return (number - 2) * multiplier
-  // }
-  return (number * multiplier);
+const countVowels = (str) => {
+  let count = 0;
+  const lowercaseStr = str.toLowerCase();
 
-
-  console.log("🚀 ~ file: main.js:72 ~ multiply ~ 'fgdfgdfg':", 'fgdfgdfg')
-}
-
-const result = multiply(4, 4)
-console.log("🚀 ~ file: main.js:73 ~ result:", result)
-console.log(multiply(3, 4));
-
-const divider = (number, divided) => number / divided
-console.log("🚀 ~ file: main.js:83 ~ divider:", divider(15, 3))
-
-const complexFunc = age => age > 18 ?
-  true :
-  false;
-
-
-console.log("🚀 ~ file: main.js:86 ~ complexFunc:", complexFunc(19))
-
-const foo = (callback) => {
-  const num = callback();
-  return num * 2
-}
-
-const bazz = () => 2
-
-console.log(foo(bazz));
-
-const multiplierExtended = (...rest) => {
-  let result = 1;
-  for (let value of rest) {
-    result *= value
+  for (let i = 0; i < lowercaseStr.length; i++) {
+    const char = lowercaseStr[i];
+    if (vowels[char]) {
+      count++;
+    }
   }
 
-  return result
-}
+  return count;
+};
 
-const mul = multiplierExtended(1, 2, 3, 4, 5, 6, 7, 8, 9)
-console.log("🚀 ~ file: main.js:111 ~ mul:", mul)
-
-const divide = (a, b) => a / b
-
-const calc = (a, b, callback) => callback(a, b);
-
-calc(4, 2, divide);
-
-
-
-// const obj = { a: 1, b: 2, c: 3 };
-
-
-const settings = { username: 'Taras' }
-
-settings.username;
-const { username } = settings;
-
-const a = obj.a;
-// const aa = obj.a
-const { a: aa = 1, b, c, d, e = 5 } = obj;
-
-const arrDesc = [1, 2]
-// const objEntries = Object.entries(obj);
-
-for (let entry of objEntries) {
-  const [key, value] = entry;
-}
-
-const descFunc = ([el1, el2, ...rest]) => {
-  debugger
-
-}
-
-descFunc(arr)
+countVowels("HeLlO");
