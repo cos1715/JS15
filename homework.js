18532f6632cde4176c5980504e293aa41f0735cd
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-09-30 16:32:15 +0300

    hw

--- homework.js
+++ homework.js
@@ -1,3 +1,5 @@
+// ## Prototype inheritance
+
 const university = {
   universityName: "Polytechnic",
 };
@@ -27,3 +29,50 @@ faculty.universityName;
 // Polytechnic
 
 enlistStudent.apply(faculty, ["Taras", "Angela", "Serhii"]);
+
+// ## Prototype constructor
+
+function Animal(name) {
+  this.name = name;
+}
+Animal.prototype.getInfo = function () {
+  return `name: ${this.name}`;
+};
+
+function Mammal(name, live) {
+  Animal.call(this, name);
+  this.live = live;
+}
+Mammal.prototype = Object.create(Animal.prototype);
+Mammal.prototype.run = function () {
+  return `${this.name} is running`;
+};
+
+function Bird(name, live) {
+  Animal.call(this, name);
+  this.live - live;
+}
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.fly = function () {
+  return `${this.name} is flying`;
+};
+
+function Dog(name, live, breed) {
+  Mammal.call(this, name, live);
+  this.breed = breed;
+}
+
+Dog.prototype.whoIsGoodDog = function () {
+  return `woof woof`;
+};
+function Penguin(name, live, species) {
+  Bird.call(this, name, live);
+  this.species = species;
+}
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.swim = function () {
+  return `${this.name} is swimming`;
+};
+Penguin.prototype.fly = function () {
+  return `penguin can't fly`;
+};
