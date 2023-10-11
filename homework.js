b42fbe798e7ab981cdca704d47b06e8911ba4451
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-10-11 19:56:16 +0300

    hw

--- homework.js
+++ homework.js
@@ -1,35 +1,109 @@
-// ## Server
+class ProductsService {
+  static #baseUrl = "https://dummyjson.com/products";
+  static getAllProducts = async () => {
+    try {
+      const resp = await fetch(this.#baseUrl);
+      const data = await resp.json();
+      return data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  static getProduct = async (id) => {
+    try {
+      const resp = await fetch(`${this.#baseUrl}/${id}`);
+      const data = await resp.json();
+      return data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  static searchProducts = async (search) => {
+    try {
+      const url = new URL(`${this.#baseUrl}/search`);
+      url.searchParams.set("q", search);
+      const resp = await fetch(url);
+      const data = await resp.json();
+      return data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  static filterProducts = async ({
+    limit = 10,
+    skip = 0,
+    select = ["title", "price"],
+  } = {}) => {
+    try {
+      const url = new URL(this.#baseUrl);
+      url.searchParams.set("limit", limit);
+      url.searchParams.set("skip", skip);
+      url.searchParams.set("select", select);
+      const resp = await fetch(url);
+      const data = await resp.json();
+      return data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  static createProducts = async (product) => {
+    try {
+      const url = new URL(`${this.#baseUrl}/add`);
+      const resp = await fetch(url, {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(product),
+      });
+      const data = await resp.json();
+      return data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  static updateProducts = async (id, product) => {
+    try {
+      const url = new URL(`${this.#baseUrl}/${id}`);
+      const resp = await fetch(url, {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(product),
+      });
+      const data = await resp.json();
+      return data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  static deleteProducts = async (id) => {
+    try {
+      const url = new URL(`${this.#baseUrl}/${id}`);
+      const resp = await fetch(url, {
+        method: "delete",
+      });
+      const data = await resp.json();
+      return data;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+}
 
-const users = [
-  {
-    id: 1,
-    name: "Taras",
-    age: 30,
-    movies: [],
-  },
-  {
-    id: 2,
-    name: "Kate",
-    age: 45,
-    movies: ["Titanic", "Avatar"],
-  },
-];
+ProductsService.getAllProducts();
+ProductsService.getProduct(1);
+ProductsService.searchProducts("phone");
+ProductsService.filterProducts();
+ProductsService.createProducts({ title: "My custom Product", price: 0.99 });
+ProductsService.updateProducts(1, { title: "My custom Product", price: 0.99 });
+ProductsService.deleteProducts(1);
 
-const getUserData = (id) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const user = users.find((item) => item.id === id);
-      if (user) {
-        resolve(user);
-      } else {
-        reject("404 not found");
-      }
-    }, 1000);
-  });
-};
-
-getUserData(11)
-  .then((user) => {
-    console.log(user);
+Promise.all([
+  ProductsService.deleteProducts(1),
+  ProductsService.deleteProducts(2),
+  ProductsService.deleteProducts(3),
+  ProductsService.deleteProducts(4),
+  ProductsService.deleteProducts(3243),
+])
+  .then((data) => {
+    console.log(data);
   })
-  .catch((e) => console.error(e));
+  .catch((e) => console.log(e));
