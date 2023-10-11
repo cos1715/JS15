4af66ab5cb4ddb4e1580b4c1e1cf17542fd81d92
Taras Samoilenko <tsamoilenko97@gmail.com>
2023-10-11 23:23:37 +0300

    lesson

--- lesson.js
+++ lesson.js
@@ -1,163 +1,43 @@
-// console.log(1);
+const subtitle = document.getElementById("subtitle");
+console.dir(subtitle);
 
-// setTimeout(() => {
-//   console.log(2);
-// }, 0);
+const container = document.querySelector(".w3-container");
+const text = container.querySelector("p");
+console.log("🚀 ~ file: lesson.js:6 ~ text:", text);
+console.log("🚀 ~ file: lesson.js:5 ~ container:", container);
 
-// Promise.resolve().then(() => {
-//   console.log(3);
-// });
+const nodeList = document.querySelectorAll(".w3-cell");
+console.log("🚀 ~ file: lesson.js:10 ~ nodeList:", nodeList);
 
-// Promise.resolve().then(() => {
-//   setTimeout(() => {
-//     console.log(4);
-//   }, 0);
-// });
+const tagCollection = document.getElementsByTagName("p");
+const classCollection = document.getElementsByClassName("w3-cell");
+console.log("🚀 ~ file: lesson.js:14 ~ classCollection:", classCollection);
+console.log("🚀 ~ file: lesson.js:13 ~ tagCollection:", tagCollection);
+const firstText = document.querySelector("div > p");
 
-// Promise.resolve().then(() => {
-//   console.log(5);
-// });
-
-// setTimeout(() => {
-//   console.log(6);
-// }, 0);
-
-// console.log(7);
-
-// 1 7 3 5 2 6 4
-
-// console.log(1);
-
-// Promise.resolve().then(() => {
-//   console.log(5);
-// });
-
-// Promise.resolve()
+// fetch("https://dummyjson.com/products")
 //   .then(() => {
-//     console.log(2);
-//     return Promise.resolve();
+//     container.classList.toggle("success");
 //   })
-//   .then(() => {
-//     console.log(3);
+//   .catch(() => {
+//     container.classList.add("error");
 //   });
 
-// console.log(4);
-
-// 1 4 5 2 3
-
-// console.log(1);
-
-// async function bazz() {
-//   console.log(2);
-//   await new Promise((resolve) => {
-//     console.log(3);
-//     setTimeout(() => {
-//       console.log(4);
-//     }, 0);
-//     console.log(5);
-//     resolve();
-//   });
-//   console.log(6);
-// }
-
-// bazz();
-
-// console.log(7);
-
-// prev result
-// 1 2 3 5 6 4
-
-// 1 2 3 5 7 6 4
-
-console.log(1);
-
-setTimeout(() => {
-  console.log(2);
-}, 0);
-
-setTimeout(() => {
-  console.log(3);
-}, 0);
-
-const promise = Promise.resolve();
-
-for (let i = 0; i < 3; i++) {
-  promise
-    .then(() => {
-      setTimeout(() => {
-        console.log(4);
-        setTimeout(() => {
-          console.log(5);
-        }, 0);
-
-        Promise.resolve().then(() => {
-          console.log(6);
-          promise.then(() => {
-            console.log(7);
-          });
-        });
-      }, 0);
-    })
-    .then(() => {
-      console.log(8);
-    });
-}
-
-console.log(9);
-
-// 1 9 8 2 3 4 6 7 5
-
-// 1 9 8 8 8 2 3 4 6 7 4 6 7 4 6 7 5 5 5
-
-const url = new URL("https://dummyjson.com/users");
-
-fetch(url)
-  .then((resp) => resp.json())
-  .then((data) => console.log(data))
-  .catch((e) => {
-    console.log(e);
-  });
-
-const getUser = async (id = 1) => {
-  const reps = await fetch(`${url}/${id}`);
-  const data = await reps.json();
-
-  console.log(data);
+const loadData = async () => {
+  container.classList.toggle("loading");
+  const resp = await fetch("https://dummyjson.com/products");
+  container.classList.toggle("success");
 };
 
-getUser();
+loadData();
 
-const getQueryUser = async () => {
-  const params = new URLSearchParams();
-  params.append("q", "John");
-  const reps = await fetch(`${url}/search?${params}`);
-  const data = await reps.json();
-
-  console.log(data);
-};
-
-getQueryUser();
-
-const createUser = async () => {
-  const user = {
-    email: "xlinster1d@bravesites.com",
-    eyeColor: "Amber",
-    firstName: "Johnathon",
-    gender: "male",
-  };
-  const reps = await fetch(`${url}/add`, {
-    method: "post",
-    headers: {
-      "Content-Type": "Application/json",
-      "X-custom-header": "123213",
-      token: "fdsfds",
-      auth: "dfdsds",
-    },
-    body: JSON.stringify(user),
-  });
-  const data = await reps.json();
-
-  console.log(data);
-};
-
-createUser();
+// const hackGithub = () => {
+//   const container = document.querySelector(
+//     "body > div.logged-in.env-production.page-responsive.full-width > div.application-main > div > div"
+//   );
+//   if (container) {
+//     container.classList.add("flex-row-reverse");
+//     const content = container.querySelector("aside + div > div > div > div");
+//     content?.classList.add("flex-row-reverse");
+//   }
+// };
